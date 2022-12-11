b9154cc926c38a3a743ec68cf392a645408aba70
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-12-11 17:04:08 -0500

    Initial Commit

---
+++ CupcakeCorner-HWS-10/CupcakeCorner_HWS_10App.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCorner_HWS_10App.swift
+//  CupcakeCorner-HWS-10
+//
+//  Created by Kevin Mattocks on 12/11/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCorner_HWS_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
