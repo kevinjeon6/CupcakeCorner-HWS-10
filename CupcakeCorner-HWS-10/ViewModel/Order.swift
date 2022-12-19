d6e47b3f4ea8cffb25aeda5a3d419adea324a587
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-12-19 16:49:02 -0500

    Create a enum with CodingKeys and use the properties that we want to encode Create an encode method that creates a container using the coding keys enum Add a required initializer to decode an instance of OrderViewModel from some archived data. This is reverse of encoding. Makes the code fully Codable

--- CupcakeCorner-HWS-10/ViewModel/Order.swift
+++ CupcakeCorner-HWS-10/ViewModel/Order.swift
@@ -7,7 +7,21 @@
 
 import Foundation
 
-class OrderViewModel: ObservableObject {
+class OrderViewModel: ObservableObject, Codable {
+    
+    enum CodingKeys: CodingKey {
+        //Properties that we want to encode
+        case type
+        case quantity
+        case extraFrosting
+        case addSprinkles
+        case name
+        case streetAddress
+        case city
+        case zip
+    }
+    
+    
     static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
     
     @Published var type = 0
@@ -41,7 +55,7 @@ class OrderViewModel: ObservableObject {
         return true
     }
     
-    
+    //MARK: - Calculating the cost
     var cost: Double {
         //$2 per cake
         
@@ -67,6 +81,43 @@ class OrderViewModel: ObservableObject {
         
         
     }
+    
+    init() {
+        //The new initializer since the required init provided errors where we wanted to create a new empty Order
+    }
+    
+    
+    /*
+     Can encode the data in any order you want - don't need to match the order in which properties are declared in your object
+     */
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        
+        try container.encode(type, forKey: .type)
+        try container.encode(quantity, forKey: .quantity)
+        try container.encode(extraFrosting, forKey: .extraFrosting)
+        try container.encode(addSprinkles, forKey: .addSprinkles)
+        try container.encode(name, forKey: .name)
+        try container.encode(streetAddress, forKey: .streetAddress)
+        try container.encode(city, forKey: .city)
+        try container.encode(zip, forKey: .zip)
+    }
+    
+    
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        type = try container.decode(Int.self, forKey: .type)
+        quantity = try container.decode(Int.self, forKey: .quantity)
+        extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+        addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+        name = try container.decode(String.self, forKey: .name)
+        streetAddress = try container.decode(String.self, forKey: .streetAddress)
+        city = try container.decode(String.self, forKey: .city)
+        zip = try container.decode(String.self, forKey: .zip)
+        
+    }
+    
 }
 
 
