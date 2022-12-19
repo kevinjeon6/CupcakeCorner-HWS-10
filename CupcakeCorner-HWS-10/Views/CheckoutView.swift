aabaab5f98922a6245212ec01d2aa1488930b82a
Kevin Mattocks <kevin.t.mattocks@gmail.com>
2022-12-19 15:51:58 -0500

    Create computed property to calculate the cost of a cupcake based on special request Create UI for CheckoutView Add an AsyncImage and resize the image

--- CupcakeCorner-HWS-10/Views/CheckoutView.swift
+++ CupcakeCorner-HWS-10/Views/CheckoutView.swift
@@ -11,12 +11,39 @@ struct CheckoutView: View {
     @ObservedObject var order: OrderViewModel
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                    //adjusting the Async image
+                    image
+                        .resizable()
+                        .scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+            }
+            
+            Text("Your total is \(order.cost, format: .currency(code: "USDD"))")
+                .font(.title)
+            
+            Button {
+                //
+            } label: {
+                Text("Place Order")
+            }
+            .padding()
+
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct CheckoutView_Previews: PreviewProvider {
     static var previews: some View {
-        CheckoutView(order: OrderViewModel())
+        NavigationView {
+            CheckoutView(order: OrderViewModel())
+        }
     }
 }
